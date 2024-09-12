98962483b277a0bf7bbe713619bc42b259396019
FelipeDosProgramas <felipeluizmsouza@gmail.com>
2024-09-12 11:08:19 -0300

    Trabalho 4 ainda não pronto

--- setup_routine/setup_database.php
+++ setup_routine/setup_database.php
@@ -1,2 +1,13 @@
 <?php
-    if (!file_exists(""))
+    /**
+     * @param string $file_path
+     * @return bool asserting if the file was created.
+     */
+    function create_if_not_exists(string $file_path): bool
+    {
+        if (file_exists($file_path))
+            return true;
+        fclose(fopen($file_path, "x+"));
+        return false;
+    }
+    if (!create_if_not_exists("./resources/database.sqlite"))
